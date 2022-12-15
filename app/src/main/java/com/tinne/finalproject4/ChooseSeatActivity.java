f3dfe41fc14d709009037e72061c21eaa7dd851c
Muhamad Anis <muhamadanis10@gmail.com>
2022-12-15 12:49:40 +0700

    Home Input + Result + DetailResult Activity

--- app/src/main/java/com/tinne/finalproject4/ChooseSeatActivity.java
+++ app/src/main/java/com/tinne/finalproject4/ChooseSeatActivity.java
@@ -1,8 +1,10 @@
 package com.tinne.finalproject4;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.content.res.AppCompatResources;
@@ -79,6 +81,11 @@ public class ChooseSeatActivity extends AppCompatActivity implements View.OnClic
 
         binding.btnBack.setOnClickListener(v -> {
             onBackPressed();
+        });
+        binding.btnSubmit.setOnClickListener(v-> {
+            Intent intent = new Intent(ChooseSeatActivity.this, HomeInputActivity.class);
+            startActivity(intent);
+            Toast.makeText(this,"Booking Succeed",Toast.LENGTH_SHORT).show();
         });
 
 
